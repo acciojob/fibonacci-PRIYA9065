a0289523e3fad4e839d177513d0e1553d524b796
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-09-01 17:32:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let a = 0, b = 1, c;
 function fibonacci(num) {
 // your code here
-	if (num == 1) return a;
+	if (num == 0) return a;
 
 	for(let i = 2; i <= num; i++){
 		c = a + b;
 		a = b;
 		b = c;
 	}
-
-	return c;
+	return b;
 }
+
 
 var n = Number(prompt("Enter Number"));
 
