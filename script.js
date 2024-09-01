0d6569e22bccd3e10d4458ace63f33f1ad5cdf8a
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-09-01 17:26:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+let a = 0, b = 1, c;
 function fibonacci(num) {
 // your code here
+	if (num == 0) return a;
+
+	for(let i = 2; i <= num; i++){
+		c = a + b;
+		a = b;
+		b = c;
+	}
+
+	return c;
 }
+
+var n = prompt("Enter Number");
+var sequence = fibonacci(n);
+alert('nth fibonacci no. = ', sequence);
 
 module.exports = fibonacci;
